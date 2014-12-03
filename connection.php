2780d3e36dc6f6ed65a2a3dcd3cb33990d50a942
ntebben <ntebben@gmail.com>
2014-12-03 13:23:55 -0500

    Consistent connection variable name

--- connection.php
+++ connection.php
@@ -4,6 +4,6 @@ $mysql_user = "root";
 $mysql_password = "";
 $mysql_database = "olio";
 $prefix = "";
-$bd = mysql_connect($mysql_hostname, $mysql_user, $mysql_password) or die("Could not connect database");
-mysql_select_db($mysql_database, $bd) or die("Could not select database");
+$conn = mysql_connect($mysql_hostname, $mysql_user, $mysql_password) or die("Could not connect database");
+mysql_select_db($mysql_database, $conn) or die("Could not select database");
 ?>
