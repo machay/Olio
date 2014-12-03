dac31a2cece9d3f73a21577bbedae79680dc1381
ntebben <ntebben@gmail.com>
2014-12-03 16:20:50 -0500

    Added new fields

--- signup.php
+++ signup.php
@@ -20,8 +20,11 @@
 							<form action="code_exec.php" method="POST">
 								<td>First Name:</td> <td> <input type="text" name="fname" required></td> </tr> 
 								<tr> <td>Last Name:</td> <td> <input type"text" name="lname" required></td> </tr>
-								<tr> <td>Institution: </td> <td> <input type="text" name = "inst"></td> </tr>
+								<tr> <td>Middle Initial:</td> <td> <input type"text" name="mi" required></td> </tr>
+								<tr> <td>Date of Birth (mm/dd/yyyy):</td> <td> <input type"date" name="dob" required></td> </tr>
 								<tr> <td>Email:</td> <td> <input type="email" name="email" required></td> </tr> 
+								<tr> <td>Institution: </td> <td> <input type="text" name = "inst"></td> </tr>
+								<tr> <td>Phone:</td> <td> <input type="tel" name="phone"></td> </tr> 
 								<tr> <td>Username:</td> <td> <input type="text" name="user" required></td> </tr> 
 								<tr> <td>Password:</td> <td> <input type="password" name="pass" required></td> </tr> 
 								<button type = "submit" class = 'btn'><a> Sign Up! </a></button>
@@ -31,3 +34,4 @@
 		</div> 
 	</body> 
 </html>
+
