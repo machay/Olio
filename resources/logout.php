3d074e7ca868027b8bc5800284f876b332ab910c
mohama4 <mohama4@rpi.edu>
2014-12-03 22:51:24 -0500

    Create logout.php

---
+++ resources/logout.php
@@ -0,0 +1,18 @@
+<?php 
+session_start();
+session_destroy();
+
+echo "You have logged out";
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+
+<link src="resources/logout.css">
+</head>
+
+<body>
+</body>
+</html>
