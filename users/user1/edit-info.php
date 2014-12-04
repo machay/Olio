dcbc8ce5c153b184dcf9279591096fd59bd9e28b
machay <machay@rpi.edu>
2014-12-04 15:31:50 -0500

    Update edit-info.php

--- users/user1/edit-info.php
+++ users/user1/edit-info.php
@@ -2,18 +2,19 @@
 session_start();
 require_once 'config.php';
 require 'connection.php';
-$yourname=mysql_real_escape_string($_POST['fname']);
-$lname=mysql_real_escape_string($_POST['lname']);
-$inst=mysql_real_escape_string($_POST['inst']);
+$fname=mysql_real_escape_string($_POST['yourname']);
+$school=mysql_real_escape_string($_POST['institution']);
+$bio = mysql_real_escape_string($_POST['bio']);
 $email=mysql_real_escape_string($_POST['email']);
-$newuser = "INSERT INTO users (fname, lname, mi, dob, email, school, phone, username, password)
-			VALUES('$fname','$lname', '$mi', '$dob', '$email', '$inst', '$phone', '$user', '$pass')";
-$mysqli->query($newuser) or die(mysqli_error($conn));
+$updateuser = "UPDATE users (fname, email, school, bio)
+			SET fname ='$fname', school='$school', 'bio'='$bio', 'email'='$email'
+			WHERE id=1";
+$mysqli->query($updateuser) or die(mysqli_error($conn));
 			//$conn->exec($newuser);
 			//$usertable = "INSERT TABLE IF NOT EXISTS "
 			//Directory manipulation here
 			$dir = '.';
 			//mkdir()
-			header("location: index.php");
+			header("location: index-redo.php");
 			mysql_close($conn);
 			?>
