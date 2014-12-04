c335b6e4d6c844c3b4fae7adc1a3189f03c67eaa
ntebben <ntebben@gmail.com>
2014-12-04 15:06:58 -0500

    Final?

--- signup.php
+++ signup.php
@@ -1,28 +1,44 @@
-			<?php
-			session_start();
-			require_once 'config.php';
-			require 'connection.php';
-			$fname=mysql_real_escape_string($_POST['fname']);
-			$lname=mysql_real_escape_string($_POST['lname']);
-			$mi=mysql_real_escape_string($_POST['mi']);
-			$dob=mysql_real_escape_string($_POST['dob']);
-			$user=mysql_real_escape_string($_POST['user']);
-			$pass=mysql_real_escape_string($_POST['pass']);
-			$inst=mysql_real_escape_string($_POST['inst']);
-			$email=mysql_real_escape_string($_POST['email']);
-			$phone=mysql_real_escape_string($_POST['phone']);
-			$newuser = "INSERT INTO users (fname, lname, mi, dob, email, school, phone, username, password)
-			VALUES('$fname','$lname', '$mi', '$dob', '$email', '$inst', '$phone', '$user', '$pass')";
-			$mysqli->query($newuser) or die(mysqli_error($conn));
-			//$conn->exec($newuser);
-			//$usertable = "INSERT TABLE IF NOT EXISTS "
-			//Directory manipulation here
-			$dir = '.';
-			//mkdir()
-<<<<<<< HEAD
-			header("location: login.html");
-=======
-			header("location: index.php");
->>>>>>> FETCH_HEAD
-			mysql_close($conn);
+	<?php
+	session_start();
+	require_once 'config.php';
+	require 'connection.php';
+	$fname=mysql_real_escape_string($_POST['fname']);
+	$lname=mysql_real_escape_string($_POST['lname']);
+	$mi=mysql_real_escape_string($_POST['mi']);
+	$dob=mysql_real_escape_string($_POST['dob']);
+	$user=mysql_real_escape_string($_POST['user']);
+	$pass=mysql_real_escape_string($_POST['pass']);
+	$inst=mysql_real_escape_string($_POST['inst']);
+	$email=mysql_real_escape_string($_POST['email']);
+	$phone=mysql_real_escape_string($_POST['phone']);
+	$newuser = "INSERT INTO users (fname, lname, mi, dob, email, school, phone, username, password)
+	VALUES('$fname','$lname', '$mi', '$dob', '$email', '$inst', '$phone', '$user', '$pass')";
+	$mysqli->query($newuser) or die(mysqli_error($conn));
+	//$conn->exec($newuser);
+	$usertable = "INSERT TABLE IF NOT EXISTS " . $_SESSION['username'] . "(
+	`id` int(11) NOT NULL,
+  	`src` varchar(255) NOT NULL
+	) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+	ALTER TABLE " . $_SESSION['username'] . "
+ 	ADD PRIMARY KEY (`id`);
+ 	ALTER TABLE " . $_SESSION['username'] . "
+	MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;";
+	//Directory manipulation here
+	$index_file = './users/user1/index.php';
+	$edit_file = './users/user1/edit.php';
+	$new_index = './users/' . $_SESSION['username'] . '/index.php';
+	$new_edit = './users/' . $_SESSION['username'] . '/edit.php';
+	$userdir = './users/' . $_SESSION['username'] .'/';
+	if (!mkdir($userdir, 0777, true)) {
+    		die('Failed to create folders...');
+    	}
+    	if (!copy($index_file, $new_index)) {
+    		echo "failed to copy $index_file...\n";
+	}
+	if (!copy($edit_file, $new_edit)) {
+    		echo "failed to copy $edit_file...\n";
+	}
+	header("location: index.php");
+	mysql_close($conn);
+
 			?>
