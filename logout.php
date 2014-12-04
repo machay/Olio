cb9b59d83f231b9e706a6971ce0ee9cf8a6085ed
mohama4 <mohama4@rpi.edu>
2014-12-03 23:13:01 -0500

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,18 +1,16 @@
-<?php 
-session_start();
-session_destroy();
-
-echo "You have logged out";
-?>
-
 <!DOCTYPE html>
 <html>
 
 <head>
-
 <link src="resources/logout.css">
 </head>
 
 <body>
+	<?php 
+	session_start();
+	session_destroy();
+
+	echo "You have logged out";
+	?>
 </body>
 </html>
