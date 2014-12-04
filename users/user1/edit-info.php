6b57d1a4d5ae3f858c5de0ec3c4b3885d0097c34
Ryan Gavin <gavinr@rpi.edu>
2014-12-04 15:32:18 -0500

    .

--- users/user1/edit-info.php
+++ users/user1/edit-info.php
@@ -1,10 +1,19 @@
 <?php
 session_start();
+<<<<<<< Updated upstream
 require_once 'config.php';
 require 'connection.php';
 $fname=mysql_real_escape_string($_POST['yourname']);
 $school=mysql_real_escape_string($_POST['institution']);
 $bio = mysql_real_escape_string($_POST['bio']);
+=======
+//require_once 'config.php';
+//require 'connection.php';
+require 'database.php';
+$yourname=mysql_real_escape_string($_POST['fname']);
+$lname=mysql_real_escape_string($_POST['lname']);
+$inst=mysql_real_escape_string($_POST['inst']);
+>>>>>>> Stashed changes
 $email=mysql_real_escape_string($_POST['email']);
 $updateuser = "UPDATE users (fname, email, school, bio)
 			SET fname ='$fname', school='$school', 'bio'='$bio', 'email'='$email'
